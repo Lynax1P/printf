a18a85d62d5f25fa9bbd5dd2c1c1bb5aeeab0674
root <root@KORSTON-STEAM-SERVER.localdomain>
2021-07-11 21:21:30 +0300

    char don't work

--- src/p_char.c
+++ src/p_char.c
@@ -3,11 +3,22 @@
 void    p_char(va_list arg, t_format *type, int *count)
 START
     char    c;
+    int     i;
 
+    i = type->wight;
     c = va_arg(arg, int);
     if (!type->wight)
     {
-
+        write(1, &c, 1);
     }
-
+    else
+    {
+        while(i)
+        {
+            write(1, " ", 1);
+            if (i - 1 == 0);
+                write(1, &c, 1);
+            i--;
+        }
+    }
 END
